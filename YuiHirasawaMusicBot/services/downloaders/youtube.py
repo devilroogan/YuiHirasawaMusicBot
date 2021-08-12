e675aae6830828b54b9252f140a5c41bd1851236
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-08-12 15:44:14 +0530

    Update youtube.py

--- YuiHirasawaMusicBot/services/downloaders/youtube.py
+++ YuiHirasawaMusicBot/services/downloaders/youtube.py
@@ -6,12 +6,12 @@ from YuiHirasawaMusicBot.config import DURATION_LIMIT
 from YuiHirasawaMusicBot.helpers.errors import DurationLimitError
 
 ydl_opts = {
-    "format": "bestaudio",
-    "addmetadata": True,
+    "format": "bestaudio/best",
+    "verbose": True,
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
-    "forceip": 4,
+    
 }
 ydl = YoutubeDL(ydl_opts)
 
