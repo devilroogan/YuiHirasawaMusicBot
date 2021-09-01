198c62d3a152c18b02a344c2b487de4da1ca2906
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-09-01 09:48:15 +0530

    Update main.py

--- YuiHirasawaMusicBot/modules/main.py
+++ YuiHirasawaMusicBot/modules/main.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, idle
 
 from YuiHirasawaMusicBot.config import API_ID, API_HASH, BOT_TOKEN
-from videoplayer import app
+from YuiHirasawaMusicBot.modules.videoplayer import app
 
 bot = Client(
     ":memory:",
